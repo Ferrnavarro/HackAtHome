f0e44f9461baab0473bac3744a850e76ccb7f928
Fernando <fer.maganavarro@gmail.com>
2017-07-18 15:22:48 -0600

    Solution

---
+++ HackAtHomeClient/EvidenceDetailActivity.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HackAtHome.SAL;
+using HackAtHomeClient.Fragments;
+using Android.Webkit;
+using Android.Graphics;
+
+namespace HackAtHomeClient
+{
+    [Activity(Label = "@string/ApplicationName", Icon = "@drawable/icon")]
+    public class EvidenceDetailActivity : Activity
+    {
+        private EvidenceDetailFragment Data;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.EvidenceDetail);
+
+           
+            Data = (EvidenceDetailFragment)this.FragmentManager.FindFragmentByTag("Data");
+            if (Data == null)
+            {
+                
+                Data = new EvidenceDetailFragment();
+                var FragmentTransaction = this.FragmentManager.BeginTransaction();
+                FragmentTransaction.Add(Data, "Data");
+                FragmentTransaction.Commit();
+            }
+
+            LoadData();
+        }
+
+        /// <summary>
+        /// Cargamos la informacion en Data para no perder la informacion.
+        /// </summary>
+        private async void LoadData()
+        {
+            
+            if (Data.Evidence == null)
+            {
+              
+                Data.FullName = Intent.GetStringExtra("FullName");
+                Data.Token = Intent.GetStringExtra("Token");
+
+                Data.Evidence = new HackAtHome.Entities.Evidence()
+                {
+                    EvidenceID = Intent.GetIntExtra("EvidenceID", 0),
+                    Title = Intent.GetStringExtra("EvidenceTitle"),
+                    Status = Intent.GetStringExtra("EvidenceStatus")
+                };
+
+                
+                var serviceClient = new ServiceClient();
+
+                try
+                {
+                    Data.Detail = await serviceClient.GetEvidenceByIDAsync(Data.Token, Data.Evidence.EvidenceID);
+                }
+                catch (Exception ex)
+                {
+                    
+                    ErrorDialog(ex.Message);
+                    Data.Detail = new HackAtHome.Entities.EvidenceDetail();
+                }
+            }
+
+          
+            var FullNameTextView = FindViewById<TextView>(Resource.Id.tvDetailFullName);
+            FullNameTextView.Text = Data.FullName;
+
+            var TitleTextView = FindViewById<TextView>(Resource.Id.tvDetailTitle);
+            TitleTextView.Text = Data.Evidence.Title;
+
+            var StatusTextView = FindViewById<TextView>(Resource.Id.tvDetailStatus);
+            StatusTextView.Text = Data.Evidence.Status;
+
+            var DescriptionWebView = FindViewById<WebView>(Resource.Id.webViewContent);
+            DescriptionWebView.LoadDataWithBaseURL(null, $"<html><head><style type='text/css'>body{{color:#FFFFFF}}</style></head><body> {Data.Detail.Description}</body></html>", "text/html", "utf-8", null);
+            
+            DescriptionWebView.SetBackgroundColor(Color.Transparent);
+            var EvidenceImageView = FindViewById<ImageView>(Resource.Id.imageViewEvidence);
+            Koush.UrlImageViewHelper.SetUrlDrawable(EvidenceImageView, Data.Detail.Url);
+        }
+
+        /// <summary>
+        /// Genera un dialog de error para la aplicacion
+        /// </summary>
+        /// <param name="title">Titulo a proporicionar </param>
+        /// <param name="iconResource">Icono del dialogo</param>
+        /// <param name="message">Mensaje a mostrar</param>
+        private void ErrorDialog(string title = "Error", int iconResource = Resource.Drawable.icon, string message = "Ocurrio un error inesperado")
+        {
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle(title);
+            Alert.SetIcon(iconResource);
+            Alert.SetMessage($"Ocurrio un error inesperado:\n{message}");
+            Alert.SetButton("Ok", (s, ev) => { });
+            Alert.Show();
+        }
+    }
+}
---
+++ HackAtHomeClient/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using HackAtHome.SAL;
+using HackAtHome.Entities;
+
+namespace HackAtHomeClient
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+
+            var validateButton = FindViewById<Button>(Resource.Id.button1);
+
+            validateButton.Click += (s, ev) =>
+            {
+                Validate();
+            };
+
+        }
+
+
+        private async void Validate()
+        {
+            ServiceClient serviceclient = new ServiceClient();
+            var emailEditText = FindViewById<EditText>(Resource.Id.editTextCorreo);
+            var passwordEditText = FindViewById<EditText>(Resource.Id.editTextPassword);
+
+
+
+            ResultInfo Result = await serviceclient.AutenticateAsync(emailEditText.Text, passwordEditText.Text);
+
+            var MicrosoftEvidence = new LabItem
+            {
+                Email = emailEditText.Text, Lab = "Hack@Home", DeviceId = Android.Provider.Settings.Secure.GetString(
+                    ContentResolver, Android.Provider.Settings.Secure.AndroidId)
+
+        };
+            var MicrosoftClient = new MicrosoftServiceClient();
+            await MicrosoftClient.SendEvidence(MicrosoftEvidence);
+
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle("Resultado de la verificacion");
+            Alert.SetIcon(Resource.Drawable.icon);
+
+
+            string message;
+
+            if (Result.Status == Status.Success || Result.Status== Status.AllSuccess)
+            {
+                message = $"Bienvenido {Result.FullName}";
+            }
+
+            else
+            {
+                message = $"Error:\n{Result.Status}\n{Result.FullName}";
+            }
+
+          
+            var Intent = new Android.Content.Intent(this, typeof(EvidenceListActivity));
+            Intent.PutExtra("FullName", Result.FullName);
+            Intent.PutExtra("Token", Result.Token);
+            StartActivity(Intent);
+        }
+
+
+    }
+}
+
